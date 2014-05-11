e828b1ed7046a2fa3c168f0c1b1c5f83a5671bc7
Nick <nick@nickswoodart.com>
2014-05-11 09:41:03 -0400

    TRACK-46: Added app namespace to url views.

--- standalone/urls.py
+++ standalone/urls.py
@@ -7,5 +7,5 @@ from gumshoe.urls import standalone_urlpatterns
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include(standalone_urlpatterns)),
+    url(r'', include(standalone_urlpatterns, app_name="gumshoe")),
 )
